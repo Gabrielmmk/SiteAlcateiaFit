360c5ed7dbe36cfe8c55d360027d296fbd69a9e0
Gabriel Medeiros <gabrielmmk087@gmail.com>
2024-10-07 18:04:04 -0300

    Redirecionando o usuario a home padrão da pagina ao fazer o reload do site

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        if(entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show')
-        } else{
+        } else {
             //entry.target.classList.remove('show')
-            
+
         }
     });
 });
+
+
+window.onload = function () {
+    if (window.location.hash) {
+        window.location.href = window.location.origin + window.location.pathname;
+    }
+};
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
